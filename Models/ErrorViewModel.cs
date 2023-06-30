bcbb47e9c3df3bd67667a42df3fc1f83cbbab115
ZachLSoftware <zacharyjlarsen@gmail.com>
2023-06-30 11:43:10 +0100

    Project Start. Template.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrainDrain.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
